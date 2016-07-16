fc5f065e0a661a6ea204b191829ea823dea1705e
ma3uk <m.gudoff@yandex.ru>
2016-07-16 18:59:51 +0500

    Remove changes

--- js/cmx.js
+++ js/cmx.js
@@ -21,6 +21,81 @@ if (!String.prototype.endsWith) {
   };
 }
 
+var ajax = {
+        send: function (data) {
+			if (typeof (data) === 'object') {
+				data.async = data.async || true;
+				data.dataType = data.dataType || 'html';
+				data.data = data.data || false;
+				data.type = data.type || 'GET';
+				
+				var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+				
+				xhr.onreadystatechange = function () {
+					if (typeof (data.beforeSend) === 'function') {
+						data.beforeSend();
+					}
+					if (xhr.readyState === 4 && xhr.status === 200) {
+						
+						if (typeof (data.success) === 'function') {
+							if (data.dataType === 'html') {
+							data.success(xhr.responseText);
+							}
+							if (data.dataType === 'xml') {
+							data.success(xhr.responseXML);
+							}
+							if (data.dataType === 'json') {
+							data.success(window.JSON.parse(xhr.responseText));
+							}
+						}
+					} else {
+						if (typeof (data.error) === 'function') {
+							data.error();
+						}
+					}
+				};
+
+                if(type = ~['GET','DELETE','POST','PUT','HEAD'].indexOf(data.type)) {
+                    if (data.type === 'GET') {
+                        xhr.open('GET',data.url + "?" + ajax.toQueryString(data.data),true);
+                        xhr.setRequestHeader('X-Requested-With','XMLHttpRequest');
+                        xhr.send(null);
+                    } else {
+                        xhr.open(data.type,data.url,true);
+                        xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+                        xhr.setRequestHeader('X-Requested-With','XMLHttpRequest');
+                        xhr.send(ajax.toQueryString(data.data));
+                    }
+                }
+			}
+		},
+			
+		toQueryString: function (data) {
+				var query = '', i,
+					push = function (key, value) {
+						query += encodeURIComponent(key) + '=' + encodeURIComponent(value) + '&';
+					}, key, value;
+
+				for (key in data) {
+					if (!Object.hasOwnProperty.call(data, key)) {
+						continue;
+					}
+					
+				value = data[key];
+				
+					if ((typeof (data) === "object") && (data instanceof Array)) {
+						for (i = 0; i < value.length; i++) {
+							push(key, value[i]);
+						}
+					} else {
+						push(key, data[key]);
+					}
+				}
+				
+				return query.replace(/&$/, '').replace(/%20/g, '+');
+			}
+	};
+
 var CMS = {
 	plugins:{},
     	addPlugin: function (nam) {
@@ -42,8 +117,8 @@ var CMS = {
 		postSnippetLength: 120,
 		pagesFolder: 'pages',
 		fadeSpeed: 300,
-		mainContainer: $('.cms_main'),
-		footerContainer: $('.cms_footer'),
+		mainContainer: document.getElementById('.cms_main'),
+		footerContainer: document.getElementById('.cms_footer'),
 		footerText: '&copy; ' + new Date().getFullYear() + ' All Rights Reserved.',
 		parseSeperator: '---',
 		loader: '<div class="loader">Loading...</div>',
@@ -274,7 +349,7 @@ var CMS = {
 			url = urlFolder + '/' + file.name;
 		}
 
-		$.ajax({
+		ajax.send({
 			type: 'GET',
 			url: url,
 			dataType: 'html',
@@ -310,7 +385,7 @@ var CMS = {
 			url = folder;
 		}
 
-		$.ajax({
+		ajax.send({
 			url: url,
 			success: function(data) {
 
